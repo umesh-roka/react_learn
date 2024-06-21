39d6190e929175e6f5e1842cb433ec958169b83c
Umesh-roka <umeshmagar764@gmail.com>
2024-06-21 16:24:08 +0545

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import RootLayout from './ui/RootLayout';
 import NotFound from './ui/NotFound';
-// import AddStdForm from './features/student/AddStdForm';
+import AddStdForm from './features/student/AddStdForm';
 import StdDetail from './features/student/StdDetail';
 import UpdateStd from './features/student/UpdateStd';
 // import UpdateForm from './features/users/UpdateForm';
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     children: [
       {path:'home',element:<Home/>},
       {path:'details', element:<StdDetail/>},
-      // { path: 'addForm', element: <AddStdForm/> },
+      { path: 'addForm', element: <AddStdForm/> },
       // { path: 'updatestd/:id', element: <UpdateStd /> },
       {path:'/updatestd/:id',element:<UpdateStd/>},
       // { path: 'about', element: <UpdateForm/> },
--- src/features/student/StdDetail.js
+++ src/features/student/StdDetail.js
@@ -1,37 +1,37 @@
-// import React from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { removeStudents } from './studentSlice';
-// import { useNavigate } from 'react-router';
-// const StdDetail = () => {
-//   const nav =useNavigate();
-//   const dispatch = useDispatch();
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeStudents } from './studentSlice';
+import { useNavigate } from 'react-router';
+const StdDetail = () => {
+  const nav =useNavigate();
+  const dispatch = useDispatch();
 
-//   const { students } = useSelector((state) => state.studentSlice);
+  const { students } = useSelector((state) => state.studentSlice);
 
-//   const handleDelete=(id)=>{
-// dispatch(removeStudents(id));
-//   }
-//   return (
-//     <div className='space-x-8 pl-5 pt-5 flex'>
-//        {students.map((student) => {
-//         return <div  key={student.id}>
-//           <h1>Name: {student.fullname}</h1>
-//           <h1>Date of Birth: {student.dob}</h1>
-//           <h1>Address: {student.address}</h1>
-//           <h1>Gender: {student.gender}</h1>
+  const handleDelete=(id)=>{
+dispatch(removeStudents(id));
+  }
+  return (
+    <div className='space-x-8 pl-5 pt-5 flex'>
+       {students.map((student) => {
+        return <div  key={student.id}>
+          <h1>Name: {student.fullname}</h1>
+          <h1>Date of Birth: {student.dob}</h1>
+          <h1>Address: {student.address}</h1>
+          <h1>Gender: {student.gender}</h1>
         
-//           <button className='bg-greeney rounded-lg mt-2  px-5 ' onClick={()=>nav(`/updatestd/${student.id}`)}>Edit</button>
-//           <button onClick={handleDelete} className='bg-red-700 rounded-lg mt-2 px-5 float-right'>Remove</button>
+          <button className='bg-greeney rounded-lg mt-2  px-5 ' onClick={()=>nav(`/updatestd/${student.id}`)}>Edit</button>
+          <button onClick={handleDelete} className='bg-red-700 rounded-lg mt-2 px-5 float-right'>Remove</button>
     
-//         </div>
-//       })}
+        </div>
+      })}
 
 
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default StdDetail
+export default StdDetail
 
 
 
